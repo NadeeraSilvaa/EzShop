a83fe3ceabfeb91e957146db7b372af7639c8412
Nadeera <nadeera.silva01@gmail.com>
2023-12-01 14:56:27 +0530

    Ez_Shop

---
+++ app/src/main/java/com/example/ezshop/fragment/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ezshop.fragment.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ezshop.R
+
+class IntroductionFragment: Fragment(R.layout.fragment_introduction){
+}
