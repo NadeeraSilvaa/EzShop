b0c52be51c93fbd4c9c8729d689ebc7b461afa1f
Nadeera <nadeera.silva01@gmail.com>
2023-12-02 23:01:55 +0530

    Ez_Shop

--- app/src/main/java/com/example/ezshop/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/ezshop/activities/LoginRegisterActivity.kt
@@ -2,11 +2,14 @@ package com.example.ezshop.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.ezshop.R
 
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(1000)
+        installSplashScreen()
         setContentView(R.layout.activity_login_register)
     }
 }
